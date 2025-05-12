2bc4d94f0981d29c572ffedab5e4811281adce19
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2025-05-12 08:47:53 +0600

    search

--- src/main/java/com/algostrivesolutionslimited/libraryappbackend/dao/BookDao.java
+++ src/main/java/com/algostrivesolutionslimited/libraryappbackend/dao/BookDao.java
@@ -1,9 +1,15 @@
 package com.algostrivesolutionslimited.libraryappbackend.dao;
 
 import com.algostrivesolutionslimited.libraryappbackend.entity.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 //@RepositoryRestResource
-public  interface BookDao extends JpaRepository<BookEntity, Long> {
+public interface BookDao extends JpaRepository<BookEntity, Long> {
+
+    Page<BookEntity> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
 }
