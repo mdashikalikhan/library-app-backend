83631fb63db233eacaf731c6cc87d453802d8cc1
mdashikalikhan <khan.ashik@gmail.com>
2025-05-19 16:04:45 +0600

    review config

--- src/main/java/com/algostrivesolutionslimited/libraryappbackend/entity/ReviewEntity.java
+++ src/main/java/com/algostrivesolutionslimited/libraryappbackend/entity/ReviewEntity.java
@@ -31,4 +31,52 @@ public class ReviewEntity {
 
     @Column(name = "review_description")
     private String reviewDescription;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getReviewDescription() {
+        return reviewDescription;
+    }
+
+    public void setReviewDescription(String reviewDescription) {
+        this.reviewDescription = reviewDescription;
+    }
 }
