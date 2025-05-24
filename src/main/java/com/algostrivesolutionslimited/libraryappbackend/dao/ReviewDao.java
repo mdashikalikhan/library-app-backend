00c6caa4b1b51c794deacc29d0235c17861be9c6
Md. Ashik Ali Khan <khan.ashik@gmail.com>
2025-05-25 00:07:04 +0600

    Review...

--- src/main/java/com/algostrivesolutionslimited/libraryappbackend/dao/ReviewDao.java
+++ src/main/java/com/algostrivesolutionslimited/libraryappbackend/dao/ReviewDao.java
@@ -1,7 +1,13 @@
 package com.algostrivesolutionslimited.libraryappbackend.dao;
 
 import com.algostrivesolutionslimited.libraryappbackend.entity.ReviewEntity;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewDao extends JpaRepository<ReviewEntity, Long> {
+
+    Page<ReviewEntity> findReviewById(@RequestParam("review_id") long reviewId, Pageable pageable);
 }
